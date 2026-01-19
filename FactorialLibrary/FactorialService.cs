78f053816ab0c30f5a2a4a4c1953b44964e44d8b
Will Fresch <wfresch@gmail.com>
2026-01-19 03:28:28 +0000

    negative number implemented

--- FactorialLibrary/FactorialService.cs
+++ FactorialLibrary/FactorialService.cs
@@ -3,6 +3,10 @@
 public class FactorialService
 {
     public static int Factorial(int n) {
+        if (n < 0) {
+            throw new InvalidOperationException("Negative numbers don't have a proper factorial!");
+        }
+        
         if (n == 0 || n == 1) {
             return 1;
         }
