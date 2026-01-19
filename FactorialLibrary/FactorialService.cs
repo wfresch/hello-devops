6983d3cdc8d28abc0e403fb4a47edd7b3cff9ec9
Will Fresch <wfresch@gmail.com>
2026-01-19 03:13:47 +0000

    initial factorial project files

---
+++ FactorialLibrary/FactorialService.cs
@@ -0,0 +1,11 @@
+﻿namespace FactorialLibrary;
+
+public class FactorialService
+{
+    public static int Factorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        return n * Factorial(n-1);
+    }
+}
